2805faf2a67a52d264d46823b61f56e5a957d8dc
Matheus Luiz Jacques <ml.jacques@hotmail.com>
2018-09-13 20:40:13 -0300

    Verificação de usuário terminado

--- RESTfulAPI/app/Traits/ApiResponser.php
+++ RESTfulAPI/app/Traits/ApiResponser.php
@@ -27,4 +27,9 @@ trait ApiResponser
     {
         return response()->json(['data' => $model], $code);
     }
+
+    protected function showMessage($message, $code = 200)
+    {
+        return response()->json(['data' => $message], $code);
+    }
 }
